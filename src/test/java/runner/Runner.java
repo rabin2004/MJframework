5f17127ca20d20b4a6ea934fb0ab8c2b8a8d7b70
rabin2004 <55056465+rabin2004@users.noreply.github.com>
2020-11-19 12:07:39 -0500

    update in runner file

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -8,8 +8,13 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="C:\\Users\\User\\Desktop\\eclipse\\MJFramework\\src\\main\\java\\features",
 				glue = "step_definations",
-				plugin = {"pretty", "html:target/cucumber_report"},
+				// usage - to check duration of execution of components, but usage can't be used along with pretty
+				// creating - html, json, junit reports
+				plugin = {"pretty", "html:target/cucumber-reports",
+						"json:target/cucumber.json", 
+						"junit:target/cucumber.xml"},
 				monochrome = true)
+
 	
 public class Runner {
 	
